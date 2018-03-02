dceb877471351eca582c3691299472e2976d616b
fangfeng <fangfeng@wacai.com>
2018-03-02 17:57:04 +0800

    修改例子支持从eureka注册中心自动同步服务进行反向代理

--- complete/book/src/main/java/hello/BookApplication.java
+++ complete/book/src/main/java/hello/BookApplication.java
@@ -2,11 +2,13 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BookApplication {
 
   @RequestMapping(value = "/available")
